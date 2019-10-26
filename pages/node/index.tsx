9cb3d1d6c1f7a7b763ff0b0530f40a0f919c6f42
ophum <inagaki0106@gmail.com>
2019-10-26 09:31:05 +0900

    [add] node一覧を表示できるようにした

---
+++ pages/node/index.tsx
@@ -0,0 +1,103 @@
+import axios from 'axios';
+import * as React from 'react';
+
+import Link from '@material-ui/core/Link';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+import Layout from '../../components/Layout';
+
+interface IAnnotation {
+	[key: string]: string,
+}
+
+interface IVirtualMachine {
+	annotations: IAnnotation[],
+	request_cpu_milli_cores: number,
+	limit_cpu_milli_cores: number,
+	request_memory_bytes: string,
+	limit_memory_bytes: string,
+}
+
+interface IReservedComputes {
+	[key: string]: IVirtualMachine,
+}
+
+interface IBlockStorage {
+	annotations: IAnnotation[],
+	request_bytes: string,
+	limit_bytes: string,
+}
+
+interface IReservedStorages {
+	[key: string]: IBlockStorage,
+}
+
+interface INode {
+	name: string,
+	address: string,
+	serial: string,
+	cpu_milli_cores: number,
+	memory_bytes: string,
+	storage_bytes: string,
+	unit: number,
+	state: string,
+	annotations: IAnnotation,
+	reserved_computes: IReservedComputes,
+	reserved_storages: IReservedStorages,
+}
+
+interface INodes {
+	nodes: INode[]
+}
+//interface IState {
+//	networks: INetwork[],
+//}
+
+const useFetch = (url: string) => {
+  const [data, updateData] = React.useState<INode[]>([]);
+
+  // empty array as second argument equivalent to componentDidMount
+  React.useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(url);
+      const data: INodes = response.data;
+      updateData(data.nodes);
+    }
+    fetchData();
+  }, [url]);
+	console.log(data);
+  return data;
+};
+
+export default function Index() {
+	const nodes = useFetch("http://172.16.14.10:8082/api/v0/node");	
+	console.log(nodes);
+	return (
+		<Layout title="n0web node">
+			<Table aria-label="network table">
+				<TableHead>
+					<TableRow>
+						<TableCell>Name</TableCell>
+						<TableCell>Address</TableCell>
+						<TableCell>Serial</TableCell>
+						<TableCell>State</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{nodes.map((node: INode) => (
+						<TableRow>
+							<TableCell><Link href="">{node.name}</Link></TableCell>
+							<TableCell>{node.address}</TableCell>
+							<TableCell>{node.serial}</TableCell>
+							<TableCell>{node.state}</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</Layout>
+	)
+}
